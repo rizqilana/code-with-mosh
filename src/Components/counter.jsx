953d0811a521219d88d7f1d6fdde9b3f870c1ead
rizqilana <muhammadrizqi54321@gmail.com>
2019-04-01 11:19:40 +0700

    Seting Atributes

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -7,8 +7,13 @@ export default class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
